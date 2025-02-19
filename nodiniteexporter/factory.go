bb9a76ec1dd61d5e4a43e2cceb8d0cff2f0b3e31
Robert Mayer <rwlmayer@gmail.com>
2025-02-19 16:49:22 +0100

    Added skeleton

---
+++ nodiniteexporter/factory.go
@@ -0,0 +1,56 @@
+package nodiniteexporter
+
+import (
+	"context"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/exporter"
+)
+
+const (
+	// typeStr is the type of the exporter
+	typeStr = "nodiniteexporter"
+)
+
+// NewFactory creates a Datadog exporter factory
+func NewFactory() exporter.Factory {
+	return exporter.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		// Uncomment the exporter type that you would like, change the second parameter as you like. Available options
+		//are listed below:
+		// component.StabilityLevelUndefined
+		// component.StabilityLevelUnmaintained
+		// component.StabilityLevelDeprecated
+		// component.StabilityLevelDevelopment
+		// component.StabilityLevelAlpha
+		// component.StabilityLevelBeta
+		// component.StabilityLevelStable
+        exporter.WithLogs(createLogsExporter, component.StabilityLevelAlpha),
+        
+	)
+}
+
+func createDefaultConfig() component.Config {
+
+	return &config{}
+}
+func createLogsExporter(
+	ctx context.Context,
+	set exporter.CreateSettings,
+	cfg component.Config,
+) (exporter.Logs, error) {
+
+	return exporterhelper.NewLogsExporter(ctx, set, cfg,
+		pushLogs,
+		//	The parameters below are optional. Uncomment any as you need.
+		//	exporterhelper.WithStart(start component.StartFunc),
+		//exporterhelper.WithShutdown(shutdown component.ShutdownFunc),
+		//exporterhelper.WithTimeout(timeoutSettings TimeoutSettings),
+		//exporterhelper.WithRetry(retrySettings RetrySettings),
+		//exporterhelper.WithQueue(queueSettings QueueSettings),
+		//exporterhelper.WithCapabilities(capabilities consumer.Capabilities)
+	)
+
+}
---
+++ nodiniteexporter/log.go
@@ -0,0 +1,16 @@
+package nodiniteexporter
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/pdata/plog"
+)
+
+// No default function for this. It must be implemented
+// Note: You can change the function name if you like
+func pushLogs(
+	ctx context.Context,
+	td plog.Logs,
+) (err error) {
+	return nil
+}
