0852ba7b1965ac1df3986795a8134804cdd1273d
Robert Mayer <rwlmayer@gmail.com>
2025-02-21 13:04:46 +0100

    Fixed test call to moqapi to verify

--- nodiniteexporter/log.go
+++ nodiniteexporter/log.go
@@ -1,7 +1,11 @@
 package nodiniteexporter
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"go.opentelemetry.io/collector/pdata/plog"
 )
@@ -12,6 +16,52 @@ func pushLogs(
 	ctx context.Context,
 	td plog.Logs,
 ) (err error) {
+
+	testurl := "https://63f32cb6fe3b595e2edc301f.mockapi.io/LogEvent"
+
+	data := map[string]int{
+		"id": 1,
+	}
+
+	// Convert the data map to a JSON string
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Create a new HTTP request
+	req, err := http.NewRequest("POST", testurl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Set the headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer YOUR_ACCESS_TOKEN") // If authentication is required
+
+	// Create an HTTP client and send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
+	if resp.StatusCode == http.StatusOK {
+		// Successfully received a response
+		var responseData map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&responseData); err != nil {
+			fmt.Println("Error decoding response:", err)
+		}
+		fmt.Println("Response:", responseData)
+	} else {
+		// Handle errors
+		fmt.Println("Error:", resp.StatusCode)
+	}
 
 	return nil
 }
